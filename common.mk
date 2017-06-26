0056d36042394c14e4630def03f51d64a5e89edb
Virgil Dupras <hsoft@hardcoded.net>
2017-06-25 21:47:58 -0400

    Deduplicate Makefile code

---
+++ common.mk
@@ -0,0 +1,31 @@
+CC = avr-gcc
+CFLAGS = -Os -DF_CPU=16000000UL -mmcu=atmega328p -c
+LDFLAGS = -mmcu=atmega328p
+
+# Patterns
+
+%.o: %.c
+	$(CC) $(CFLAGS) -o $@ $<
+
+%.bin:
+	$(CC) $(LDFLAGS) $< -o $@
+
+%.hex: %.bin
+	avr-objcopy -O ihex -R .eeprom $< $@
+
+# Rules
+
+.PHONY: send clean
+
+all: $(PROGNAME).hex
+	@echo Done!
+
+send: $(PROGNAME).hex
+	avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:$(PROGNAME)
+
+clean:
+	rm *.o *.hex *.bin
+
+$(PROGNAME).hex: $(PROGNAME).bin
+$(PROGNAME).bin: main.o
+main.o: main.c
