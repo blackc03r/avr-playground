5e2619595605e58fe2dea6d1a85a6441bcb58368
Virgil Dupras <hsoft@hardcoded.net>
2017-07-07 10:29:26 -0400

    make lcdknob work!
    Finally, we're having some real action here. "Hello!" being printed to
    the screen. For some reason though, there's a space being inserted in
    the middle of the word. No idea why yet.
    
    A big advantage we have here compared to Arduino's "LiquidCrystal"
    library is that we properly use the BF flag so that we don't have to
    wait for the screen *maximal* command process time. We can wait for
    precisely the time it takes for the command to be processed.

--- lcdknob/main.c
+++ lcdknob/main.c
@@ -3,86 +3,92 @@
 #include <stdbool.h>
 
 #include "../common/util.h"
+#include "../common/pin.h"
 
 /* A LCD screen with its content being controlled by buttons and a knob.
  */
 
-#define RS_HIGH sbi(PORTB, PORTB3)
-#define RS_LOW cbi(PORTB, PORTB3)
-#define RW_HIGH sbi(PORTB, PORTB4)
-#define RW_LOW cbi(PORTB, PORTB4)
-#define E_HIGH sbi(PORTB, PORTB2)
-#define E_LOW cbi(PORTB, PORTB2)
-#define DB0_HIGH sbi(PORTB, PORTB1)
-#define DB0_LOW cbi(PORTB, PORTB1)
-#define DB1_HIGH sbi(PORTB, PORTB0)
-#define DB1_LOW cbi(PORTB, PORTB0)
-#define DB2_HIGH sbi(PORTD, PORTD7)
-#define DB2_LOW cbi(PORTD, PORTD7)
-#define DB3_HIGH sbi(PORTD, PORTD6)
-#define DB3_LOW cbi(PORTD, PORTD6)
-#define DB4_HIGH sbi(PORTD, PORTD5)
-#define DB4_LOW cbi(PORTD, PORTD5)
-#define DB5_HIGH sbi(PORTD, PORTD4)
-#define DB5_LOW cbi(PORTD, PORTD4)
-#define DB6_HIGH sbi(PORTD, PORTD3)
-#define DB6_LOW cbi(PORTD, PORTD3)
-#define DB7_HIGH sbi(PORTD, PORTD2)
-#define DB7_LOW cbi(PORTD, PORTD2)
+#define PinRS PinD2
+#define PinRW PinD3
+#define PinE PinD4
+#define PinDB0 PinD5
+#define PinDB1 PinD6
+#define PinDB2 PinD7
+#define PinDB3 PinB0
+#define PinDB4 PinB1
+#define PinDB5 PinB2
+#define PinDB6 PinB3
+#define PinDB7 PinB4
+#define PinLED PinB5
+
+static unsigned char data_pins[8] = {
+    PinDB0,
+    PinDB1,
+    PinDB2,
+    PinDB3,
+    PinDB4,
+    PinDB5,
+    PinDB6,
+    PinDB7,
+};
+
+void write8bit(unsigned char val)
+{
+    char i;
+    for (i=0; i<8; i++) {
+        if (val & (1 << i)) {
+            pinhigh(data_pins[i]);
+        } else {
+            pinlow(data_pins[i]);
+        }
+    }
+}
 
 void clearbytes()
 {
-    DB0_LOW;
-    DB1_LOW;
-    DB2_LOW;
-    DB3_LOW;
-    DB4_LOW;
-    DB5_LOW;
-    DB6_LOW;
-    DB7_LOW;
+    write8bit(0);
 }
 
 void debugLED(bool on)
 {
     if (on) {
-        sbi(PORTB, PORTB5);
+        pinhigh(PinLED);
     } else {
-        cbi(PORTB, PORTB5);
+        pinlow(PinLED);
     }
 }
 
 void pulseEnable()
 {
-    E_LOW;
+    pinlow(PinE);
     _delay_us(1);
-    E_HIGH;
+    pinhigh(PinE);
     _delay_us(1);
-    E_LOW;
-    _delay_us(1);
+    pinlow(PinE);
 }
 
 // Returns whether the BF flag was ON at least for one cycle
-void wait_for_bf(bool expect)
+void wait_for_bf()
 {
-    RW_HIGH;
-    cbi(DDRD, DDD2);
-    if (expect) {
-        while (!(PIND & _BV(PIND2))) {
-            pulseEnable();
-        }
-    } else {
-        while (PIND & _BV(PIND2)) {
-            pulseEnable();
-        }
+    // We don't use pulseEnable() because PinE has to be HIGH when we read the BF flag.
+    pinhigh(PinRW);
+    pininputmode(PinDB7);
+    pinhigh(PinE);
+    _delay_us(1);
+    while (pinishigh(PinDB7)) {
+        pinlow(PinE);
+        _delay_us(1);
+        pinhigh(PinE);
+        _delay_us(1);
     }
-    sbi(DDRD, DDD2);
-    RW_LOW;
+    pinlow(PinE);
+    pinoutputmode(PinDB7);
+    pinlow(PinRW);
 }
 
 void pulseEnableAndWaitBF()
 {
     pulseEnable();
-    wait_for_bf(true);
     wait_for_bf(false);
 }
 
@@ -96,143 +102,71 @@ void blink_forever(bool fast)
     }
 }
 
-/* void write4bits(uint8_t value)                            */
-/* {                                                         */
+void sendcmd(unsigned char cmd)
+{
+    pinlow(PinRS);
+    write8bit(cmd);
+    pulseEnableAndWaitBF();
+}
 
-/*     for (int i = 0; i < 4; i++) {                         */
-/*         digitalWrite(_data_pins[i], (value >> i) & 0x01); */
-/*     }                                                     */
-
-/*     pulseEnable();                                        */
-/* }                                                         */
-
-/* void write8bits(uint8_t value)                            */
-/* {                                                         */
-/*     for (int i = 0; i < 8; i++) {                         */
-/*         digitalWrite(_data_pins[i], (value >> i) & 0x01); */
-/*     }                                                     */
-
-/*     pulseEnable();                                        */
-/* }                                                         */
-
-/* void send(uint8_t value, uint8_t mode)                     */
-/* {                                                          */
-/*     digitalWrite(_rs_pin, mode);                           */
-
-/*     // if there is a RW pin indicated, set it low to Write */
-/*     if (_rw_pin != 255) {                                  */
-/*         digitalWrite(_rw_pin, LOW);                        */
-/*     }                                                      */
-
-/*     if (_displayfunction & LCD_8BITMODE) {                 */
-/*         write8bits(value);                                 */
-/*     } else {                                               */
-/*         write4bits(value>>4);                              */
-/*         write4bits(value);                                 */
-/*     }                                                      */
-/* }                                                          */
-
-
-void LEDDance() {
-    _delay_ms(1000);
-    DB7_HIGH;
-    _delay_ms(1000);
-    DB7_LOW;
-    _delay_ms(1000);
-    DB6_HIGH;
-    _delay_ms(1000);
-    DB6_LOW;
-    _delay_ms(1000);
-    DB5_HIGH;
-    _delay_ms(1000);
-    DB5_LOW;
-    _delay_ms(1000);
-    DB4_HIGH;
-    _delay_ms(1000);
-    DB4_LOW;
-    _delay_ms(1000);
-    DB3_HIGH;
-    _delay_ms(1000);
-    DB3_LOW;
-    _delay_ms(1000);
-    DB2_HIGH;
-    _delay_ms(1000);
-    DB2_LOW;
-    _delay_ms(1000);
-    DB1_HIGH;
-    _delay_ms(1000);
-    DB1_LOW;
-    _delay_ms(1000);
-    DB0_HIGH;
-    _delay_ms(1000);
-    DB0_LOW;
-    _delay_ms(1000);
-    E_HIGH;
-    _delay_ms(1000);
-    E_LOW;
-    _delay_ms(1000);
-    RS_HIGH;
-    _delay_ms(1000);
-    RS_LOW;
-    _delay_ms(1000);
-    RW_HIGH;
-    _delay_ms(1000);
-    RW_LOW;
+void sendchar(unsigned char c)
+{
+    pinhigh(PinRS);
+    write8bit(c);
+    pulseEnableAndWaitBF();
 }
 
 int main()
 {
-    DDRB |= _BV(DDB5); // Debug LED
-
-    debugLED(true);
-    wait_for_bf(true);
-    debugLED(false);
-
-    DDRB |= _BV(DDB3); // LED RS
-    DDRB |= _BV(DDB4); // LED RW
-    DDRB |= _BV(DDB2); // LED E
-    DDRB |= _BV(DDB1); // LED DB0
-    DDRB |= _BV(DDB0); // LED DB1
-    DDRD |= _BV(DDD7); // LED DB2
-    DDRD |= _BV(DDD6); // LED DB3
-    DDRD |= _BV(DDD5); // LED DB4
-    DDRD |= _BV(DDD4); // LED DB5
-    DDRD |= _BV(DDD3); // LED DB6
-    DDRD |= _BV(DDD2); // LED DB7
+    pinoutputmode(PinLED);
+    pinoutputmode(PinRS);
+    pinoutputmode(PinE);
+    pinoutputmode(PinRW);
+    pinoutputmode(PinDB0);
+    pinoutputmode(PinDB1);
+    pinoutputmode(PinDB2);
+    pinoutputmode(PinDB3);
+    pinoutputmode(PinDB4);
+    pinoutputmode(PinDB5);
+    pinoutputmode(PinDB6);
+    pinoutputmode(PinDB7);
     clearbytes();
+    pinlow(PinRS);
+    pinlow(PinE);
 
-    RS_LOW;
-    E_LOW;
-
-    wait_for_bf(false);
-
-    // display clear
-    /* DB0_HIGH;               */
-    /* pulseEnableAndWaitBF(); */
-    /* clearbytes();           */
-
-    // function set
-    /* DB5_HIGH;               */
-    /* DB4_HIGH;               */
-    /* pulseEnableAndWaitBF(); */
-    /* clearbytes();           */
-
-    /* _delay_ms(1000); */
+    // Initialization
+    _delay_ms(40);
+    write8bit(0b00110000);
+    pulseEnable();
+    _delay_ms(5);
+    pulseEnable();
+    _delay_us(100);
 
     // Display mode
-    DB3_HIGH;
-    /* DB2_HIGH; */
-    /* DB1_HIGH; */
-    pulseEnableAndWaitBF();
-    clearbytes();
+    sendcmd(0b00001110);
 
-    blink_forever(true);
+    // display clear
+    sendcmd(0b00000001);
 
     // entry mode
-    DB2_HIGH;
-    DB1_HIGH;
-    pulseEnable();
+    sendcmd(0b00000110);
+
+    // Display mode
+    sendcmd(0b00001111);
+
+    // write text
+    sendchar('H');
+    sendchar('e');
+    sendchar('l');
+    sendchar('l');
+    sendchar('o');
+    sendchar('!');
+
+    // All pins low
     clearbytes();
+    pinlow(PinRS);
+
+    blink_forever(true);
 
     // Return home
     /* DB1_HIGH;      */
@@ -240,10 +174,10 @@ int main()
     /* clearbytes();  */
 
     // Set character
-    RS_HIGH;
-    DB6_HIGH;
-    DB3_HIGH;
-    pulseEnable();
-    clearbytes();
-    RS_LOW;
+    /* RS_HIGH;       */
+    /* DB6_HIGH;      */
+    /* DB3_HIGH;      */
+    /* pulseEnable(); */
+    /* clearbytes();  */
+    /* RS_LOW;        */
 }
