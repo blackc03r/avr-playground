be3e5ceaf9de314c4fa1734720da012f843de581
Virgil Dupras <hsoft@hardcoded.net>
2019-03-31 13:18:12 -0400

    serial: make example more self-contained

--- serial/main.c
+++ serial/main.c
@@ -1,7 +1,31 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdio.h>
-#include "../common/uart.h"
+
+static void uart_putchar(char c)
+{
+    if (c == '\n') {
+        uart_putchar('\r');
+    }
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = c;
+}
+
+static void uart_putstr(const char *s)
+{
+    while (*s != '\0') {
+        uart_putchar(*s);
+        s++;
+    }
+}
+
+// function to initialize UART
+static void uart_init(unsigned long int baud)
+{
+    unsigned int baudrate = F_CPU/(baud*16UL)-1;
+    UBRR0H = (baudrate>>8);             // shift the register right by 8 bits
+    UBRR0L = baudrate;                  // set baud rate
+    UCSR0B|= (1<<TXEN0)|(1<<RXEN0);     // enable receiver and transmitter
+}
 
 /* Outputs "Hello World!" to the serial console every second.
  */
@@ -9,7 +33,7 @@ int main (void)
 {
     uart_init(9600);
     while (1) {
-        printf("Hello world!\n");
+        uart_putstr("Hello World!\n");
         _delay_ms(1000);
     }
     return 0;
